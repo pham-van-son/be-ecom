702a06f885f0a90f60ca2966bf1c81c5b198ed17
Phạm Hồng Sơn <vansonpham10112k1@gmail.com>
2025-11-13 15:39:34 +0700

    update-config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import * as dotenv from 'dotenv';
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { MongooseModule } from '@nestjs/mongoose';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MulterModule.register({
+      dest: './public/uploads',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecom'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true });
+
+  app.setGlobalPrefix('api/v1');
+
+  app.use(bodyParser.json({ limit: '1000mb' }));
+  app.use((bodyParser.urlencoded({ limit: '1000mb', extended: true })));
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use('/uploads', express.static(join(__dirname, '..', 'public', 'uploads')));
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
