2a98d5b0e5233c8ca117a8d48521decd2da11a2f
Hồng Sơn <vansonpham10112k1@gmail.com>
2025-11-17 08:06:17 +0700

    update-config-database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationModule } from './common/utils/location/location.module';
 import { UploadImagesModule } from './common/utils/upload-images/upload-images.module';
 import { UploadVideosModule } from './common/utils/upload-videos/upload-videos.module';
+import ormConfig from './common/config/typeorm.config';
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ dotenv.config();
     MulterModule.register({
       dest: './public/uploads',
     }),
-    TypeOrmModule.forRootAsync({}),
+    TypeOrmModule.forRoot(ormConfig),
     LocationModule,
     UploadImagesModule,
     UploadVideosModule,
---
+++ src/common/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+
+const ormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'ecom_postgres',
+  port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  username: process.env.POSTGRES_USER || 'ecom_user_hongson',
+  password: process.env.POSTGRES_PASSWORD || 'ecom_password_hongson',
+  database: process.env.POSTGRES_DB || 'ecom_db',
+
+  entities: [join(__dirname, '../**/**/*.entity.{ts,js}')],
+
+  synchronize: false,
+  migrationsRun: true,
+  migrations: [join(__dirname, '../migrations/*.{ts,js}')],
+
+  logging: true,
+};
+
+export default ormConfig;
