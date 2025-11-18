f6376c0200a35a09e595bc4bab6f0d3306a69095
Phạm Hồng Sơn <vansonpham10112k1@gmail.com>
2025-11-18 08:41:41 +0700

    update-format

--- src/common/middlewares/admin.middleware.ts
+++ src/common/middlewares/admin.middleware.ts
@@ -1,34 +1,33 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { JwtService } from "@nestjs/jwt";
-import { Observable } from "rxjs";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate {
-    constructor(
-        private jwtService: JwtService,
-        private reflector: Reflector,
-    ) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const request = context.switchToHttp().getRequest();
-        const token = request.headers.authorization?.split(' ')[1];
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
 
-        if (!token) {
-            return false;
-        }
-
-        try {
-            const payload = this.jwtService.verify(
-                token,
-                {
-                    secret: process.env.ADMIN_SECRET_KEY,
-                }
-            );
-            request.admin = payload;
-            return true;
-        } catch (error) {
-            return false;
-        }
+    if (!token) {
+      return false;
     }
+
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: process.env.ADMIN_SECRET_KEY,
+      });
+      request.admin = payload;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 }
